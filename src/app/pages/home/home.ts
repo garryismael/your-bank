77516a4370f8df6a1151577db7b0f57825f2f77a
garry <tahinjanaharygarry@gmail.com>
2025-10-04 08:11:00 +1400

    add OurProducts component with template, styles, and tests; update Home component to include OurProducts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from "../../components/hero/hero";
+import { OurProducts } from "../../components/our-products/our-products";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero],
+  imports: [Hero, OurProducts],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
